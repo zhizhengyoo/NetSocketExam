fa4bd1702402ff4ff124430d5e12af1df303e857
10975 <zhizhengyang@hand-china.com>
2016-08-01 22:37:17 +0800

    modify

--- NetSocketExam/Exam3/src/main/java/com/hand/App.java
+++ NetSocketExam/Exam3/src/main/java/com/hand/App.java
@@ -1,16 +1,7 @@
 package com.hand;
 import java.io.*;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 
-import javax.swing.text.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -20,45 +11,106 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.*;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;;
 
+class Tell{
+	public String[] Mss() throws IOException {
+		URL url;
+		url = new URL("http://hq.sinajs.cn/list=sz300170");
+		URLConnection conn = url.openConnection();
+		InputStream is = conn.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br =  new BufferedReader(isr);
 
+		String line ;
+		StringBuilder builder = new StringBuilder();
+		while((line=br.readLine())!=null){
+			builder.append(line);
+		}
+		br.close();
+		isr.close();
+		is.close();
+		String str = builder.toString(); 
+		String[] mess = str.split(",");
+		String[] name = mess[0].split("\"");
+		mess[0] = name[1];
+
+		return mess;
+	}
+}
+
+class XmlTrans{
+	public XmlTrans(String[] res) {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory. newInstance ();
+		DocumentBuilder db;
+
+		try {
+			db = dbf .newDocumentBuilder ();
+			Document document = db .newDocument () ;
+			Element root = document .createElement( "stock") ;
+			Element name1 = document .createElement("name");
+			Element open = document .createElement( "open");
+			Element close = document .createElement( "close") ;
+			Element current = document .createElement( "current") ;
+			Element high = document .createElement( "high") ;
+			Element low = document .createElement( "low") ;
+
+			name1 .setTextContent (res[0]);
+			open.setTextContent (res[1]);
+			close .setTextContent (res[2]);
+			current.setTextContent (res[3]);
+			high .setTextContent (res[4]);
+			low .setTextContent (res[5]);
+
+			root.appendChild(name1);
+			root.appendChild(open);
+			root.appendChild(close);
+			root.appendChild(current);
+			root.appendChild(high);
+			root.appendChild(low);
+			document.appendChild(root);
+
+			TransformerFactory tf = TransformerFactory . newInstance() ;
+			Transformer t;
+			t = tf .newTransformer ();
+			StringWriter writer =new StringWriter ();
+			t.transform(new DOMSource(document),new StreamResult(writer));
+//			System.out.println(writer.toString());            
+			t .transform ( new DOMSource (document ) ,new StreamResult( new File ("hand.xml" ))) ;  //这种方法在工程目录内创建了一个XML文件
+
+		} catch (ParserConfigurationException e1) {
+			e1.printStackTrace();
+		}catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
 
 public class App 
 {
 	public static void main( String[] args )
 	{
-		URL url;
-		try {
-			url = new URL("http://hq.sinajs.cn/list=sz300170");
-			URLConnection conn = url.openConnection();
-			InputStream is = conn.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br =  new BufferedReader(isr);
 
-			String line ;
-			StringBuilder builder = new StringBuilder();
-			while((line=br.readLine())!=null){
-				builder.append(line);
-			}
-			br.close();
-			isr.close();
-			is.close();
-			String str = builder.toString(); 
-			args = str.split(",");
-			String[] name = args[0].split("\"");
-			args[0] = name[1];
-			for (int i = 0; i < args.length; i++) {
-				System.out.println(args[i]);
-			}
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
+		Tell re = new Tell();
+	
+
+		try {
+			String[] res = re.Mss();
+	        new XmlTrans(res);
+
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
+
+
+
+
 	}
-}
+}	
