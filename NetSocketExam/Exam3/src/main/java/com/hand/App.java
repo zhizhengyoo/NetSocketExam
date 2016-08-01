8bb550abde0930afd24dccaaf63f68d3640506df
10975 <zhizhengyang@hand-china.com>
2016-08-01 23:35:55 +0800

    modify

--- NetSocketExam/Exam3/src/main/java/com/hand/App.java
+++ NetSocketExam/Exam3/src/main/java/com/hand/App.java
@@ -13,7 +13,8 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;;
+import org.w3c.dom.Element;
+
 
 class Tell{
 	public String[] Mss() throws IOException {
@@ -77,9 +78,8 @@ class XmlTrans{
 			t = tf .newTransformer ();
 			StringWriter writer =new StringWriter ();
 			t.transform(new DOMSource(document),new StreamResult(writer));
-//			System.out.println(writer.toString());            
-			t .transform ( new DOMSource (document ) ,new StreamResult( new File ("hand.xml" ))) ;  //杩欑鏂规硶鍦ㄥ伐绋嬬洰褰曞唴鍒涘缓浜嗕竴涓猉ML鏂囦欢
-
+			//			System.out.println(writer.toString());            
+			t .transform ( new DOMSource (document ) ,new StreamResult( new File ("hand.xml" ))); 
 		} catch (ParserConfigurationException e1) {
 			e1.printStackTrace();
 		}catch (TransformerConfigurationException e) {
@@ -91,17 +91,70 @@ class XmlTrans{
 	}
 }
 
+
+//class JsonC{
+//	public JsonC(String[] res) {
+//		// TODO Auto-generated constructor stub
+//		JsonObject jo = new JsonObject() ;
+//		JsonArray ja = new JsonArray () ;
+//		JsonObject lan1 = new JsonObject () ;
+//		lan1 .addProperty ( "name", res[0] );
+//		lan1 .addProperty ( "open", res[1] );
+//		lan1 .addProperty ( "close", res[2] );
+//		lan1 .addProperty ( "current", res[3] );
+//		lan1 .addProperty ( "high", res[4] );
+//		lan1 .addProperty ( "low", res[5] );
+//
+//		ja.add ( lan1) ;
+//		jo .add ( "stock", ja );
+//		
+//		
+//		
+//		File file = new File("hand.json");
+//		FileOutputStream fos;
+//		try {
+//			fos = new FileOutputStream(file);
+//			OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+//			osw.write(jo.toString());
+//			osw.flush();
+//			osw.close();
+//			fos.close();
+//
+//
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//
+//	}
+//}
+
+
 public class App 
 {
 	public static void main( String[] args )
 	{
 
 		Tell re = new Tell();
-	
+
 
 		try {
 			String[] res = re.Mss();
-	        new XmlTrans(res);
+			if(new XmlTrans(res) != null){
+				System.out.println("XML文件创建成功");
+			}
+//			if (new JsonC(res)!=null) {
+//				System .out . println("Json文件创建成功");
+//
+//			}
+
 
 
 		} catch (IOException e) {
